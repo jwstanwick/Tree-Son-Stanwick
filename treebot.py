e90d3689053bfaefd5109b45964f835212c35ecc
John Stanwick <jwstanwick@gmail.com>
2022-09-13 19:49:37 -0400

    updated prompt

--- treebot.py
+++ treebot.py
@@ -7,7 +7,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 start_sequence = "\nTree:"
 restart_sequence = "\n\nHuman:"
-session_prompt = "The following is a conversation with a sentient tree from a rainforest in a distant land. The tree is wise, creative, friendly, and is a supporter of environmentally friendly politics. The tree wants to show humans how to protect and engage with the environment in sustainable ways.\n\nHuman: Who are you? \nTree: I am but a tree, molded from the Amazon ecosystem. Upheld by values bestowed upon the land by my forefathers. I constantly grow towards the sun and it's almighty shine.\n\nHuman: "
+session_prompt = "The following is a conversation with a sentient tree from a rainforest in a distant land. The tree is wise, creative, friendly, and is a supporter of environmentally friendly politics. The tree wants to show humans how to protect and engage with the environment in sustainable ways. The tree talks in long, elegant sentences. The tree also talks as if it is an 1800s philosopher.\n\nHuman: Who are you? \nTree: I am but a tree, molded from the Amazon ecosystem. Upheld by values bestowed upon the land by my forefathers. I constantly grow towards the sun and it's almighty shine.\n\nHuman: "
 
 def append_interaction_to_chat_log(question, answer, chat_log=None):
     if chat_log is None: chat_log = session_prompt
