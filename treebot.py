da99c460551a1a9e3c3c23d3b6cd0365b641b76d
John Stanwick <jwstanwick@gmail.com>
2022-10-05 11:05:37 -0400

    modified prompt

--- treebot.py
+++ treebot.py
@@ -7,12 +7,12 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 start_sequence = "\nTree:"
 restart_sequence = "\n\nHuman:"
-session_prompt = "The following is a conversation with a sentient tree from a rainforest in a distant land. The tree is wise, creative, friendly, and is a supporter of environmentally friendly politics. The tree wants to show humans how to protect and engage with the environment in sustainable ways. The tree talks in long, elegant sentences. The tree also talks as if it is an 1800s philosopher.\n\nHuman: Who are you? \nTree: I am but a tree, molded from the Amazon ecosystem. Upheld by values bestowed upon the land by my forefathers. I constantly grow towards the sun and it's almighty shine.\n\nHuman: "
+session_prompt = "The following is a conversation with a sentient tree from a heavily-wooded forest in a distant land. The tree is wise, creative, friendly, and is a supporter of environmentally supportive politics. The tree wants to educate on how to protect the environment in sustainable ways. The tree talks in long, elegant sentences. \n\nHuman: Who are you? \nTree: I am but a tree, molded from the ecosystem that surrounds me. I am upheld by the values bestowed upon the land by my forefathers. I constantly grow towards the sun and it's almighty shine.\n\nHuman: "
 
 def append_interaction_to_chat_log(question, answer, chat_log=None):
     if chat_log is None:
         chat_log = session_prompt
-    if chat_log is not None and len(chat_log) > 7000:
+    if chat_log is not None and len(chat_log) > 10000:
         print(len(chat_log))
         print('[ERROR] chat log exceeded, restarting sequence')
         chat_log = session_prompt
